0427cce421842920b6fcde507b1fb240afe98b8e
Ezequielagradnik <ezequielagradnik@gmail.com>
2024-11-05 19:44:14 -0300

    instale algunasd cosas

---
+++ dbconfig.js
@@ -0,0 +1,14 @@
+import pg from "pg";
+
+const { Pool } = pg;
+
+
+
+export const pool = new Pool({
+    user: "default",
+    host: "ep-late-dream-a47fp0wv-pooler.us-east-1.aws.neon.tech",
+    database: "verceldb",
+    password: "ha1HAPemuLj2" ,
+    port: 5432,
+    ssl: true
+});
